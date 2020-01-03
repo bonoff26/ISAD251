4736aff0703ec53945fbcd5ea6cec38d28d35f14
Tallenbrook <tom.allenbrook@gmail.com>
2020-01-03 20:17:09 +0000

    Landing page setup

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once "../assets/js/bootstrap.html";
+?>
+
+<link rel="stylesheet" href="../assets/css/background.css">
+<link rel="stylesheet" href="../assets/css/landing.css">
+
+<nav class="navbar fixed-top navbar-light bg-light">
+  <a class="navbar-brand" href="#">Fixed top</a>
+</nav>
+
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+    <div class="buttons">
+        <a class="btn btn-secondary" id="1" href="#" role="button">Drinks Menu</a>
+        <a class="btn btn-secondary" id="2" href="#" role="button">Snacks Menu</a>
+    </div>
+</div>
